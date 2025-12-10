b9db6591fdd431d100a4a83d068238593fd0c571
LarrySon786 <richardsbrandon3@gmail.com>
2025-12-10 12:28:21 -0500

    Fixing for render.com

--- src/js/explore.mjs
+++ src/js/explore.mjs
@@ -1,21 +1,2 @@
-// Import Relevant functions and variables
 
-import { fetchApiData } from "./nasa-api.mjs";
-import { buildLocationsDisplay } from "./utils.mjs"
-// // const apiKey = import.meta.env.NASA_API;
-// const apiUrlNasaAstronomyPhotos = "https://images-api.nasa.gov/asset/";
-// const NASA_API = 'tbSzsCqBm4Ss1693lTx1giVto8G1tFCvaf9IrJwY';
-// const astronomyPhotos = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API}`
-// const NASA_ID = 'fdd50c79-6345-4cbc-a7a4-29b4fd6f7b20';
-
-
-// Call function to retrieve data from NASA API
-// const nasaData = fetchApiData(apiUrlNasaAstronomyPhotos);
-
-// Call function to build the page using NASA's APIs
-// buildLocationsDisplay(nasaData);
-
-
-
-buildLocationsDisplay();
 
